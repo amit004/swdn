b3f4a28048bf49c1105d7ebcbad104e124642879
amit004 <amitkumarsharma004@gmail.com>
2017-07-08 23:51:15 +0530

    db changes pushed

---
+++ SqlWrapper/DB_WRAPPER_2.sql
@@ -0,0 +1,5 @@
+ ALTER TABLE tbl_sept_activity CHANGE answer_text answer_text varchar(255);
+
+ ALTER TABLE tbl_sept_activity CHANGE right_ans_text right_ans_text varchar(255);
+
+ Alter table usr_sept_status  CHANGE  user_id  student_id bigint(11);
